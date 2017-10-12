2372863970d57d9ff8b2de7364c7f72b043d54ae
ArianZargaran <arianzf@gmail.com>
2017-10-11 21:10:50 -0700

    project completed

--- js/main.js
+++ js/main.js
@@ -1,17 +1,16 @@
 function displayInfo(){
  var nameIn = document.getElementById("name").value; //stores the value of the Name form field
  var messageIn = document.getElementById("message").value; //stores the value of the Message form field
-
- var userNameText = document.createTextNode("Username: " + nameIn); //creates the text that goes inside the H2 tag
- var commentText = document.createTextNode("Comment: " + messageIn); //creates the text that goes inside the P tag
- var commentImage = document.createElement('IMG'); //Creates the img tag
-
  var commentWrapper = document.getElementById('comments'); //calling the section where we are going to display the elements
+
+ var userNameText = document.createTextNode(nameIn); //creates the text that goes inside the H2 tag
+ var commentText = document.createTextNode(messageIn); //creates the text that goes inside the P tag
+ var commentImage = document.createElement('IMG'); //Creates the img tag
+     commentImage.className = 'image'; //gives IMG style attributes
 
 //Each comment block
  var newElem = document.createElement('DIV'); //Creates the DIV tag
      newElem.className = "newClass"; //includes a class to the element that is affected by our css file
-     commentImage.className = 'image'; //gives IMG style attributes
 
  var textArea = document.createElement('DIV'); //Creates the DIV tag
      textArea.className = "textArea"; //includes a class to the element that is affected by our css file
@@ -20,9 +19,8 @@ function displayInfo(){
      userName.appendChild(userNameText); //includes the text (NAME) to our H2
 
  var newComment = document.createElement("P"); //Creates the P tag
-	 newComment.appendChild(commentText); //includes the text (COMMENT) to our P
+	   newComment.appendChild(commentText); //includes the text (COMMENT) to our P
      commentImage.setAttribute('src', 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e0/SNice.svg/1200px-SNice.svg.png'); //Sets the attribute to the image
-
 
 //APPENDS
   document.getElementById("comments").appendChild(newElem);
@@ -31,6 +29,23 @@ function displayInfo(){
   textArea.appendChild(userName);
   textArea.appendChild(newComment);
 
+//SHOW CLEAR BUTTON
+  var clearButton = document.getElementById("clear");
+  clearButton.classList.add("clear_display");
+
 //FORM RESET
   document.getElementById("form").reset();
-}
+};
+
+
+function cleanComments() {
+   var commentsSection = document.getElementById("comments").getElementsByClassName("newClass");
+   var len = commentsSection.length;
+
+   for (var i = len-1; i >= 0; i--) {
+    commentsSection[i].parentNode.removeChild(commentsSection[i]);
+    }
+
+   var clearButton = document.getElementById("clear");
+       clearButton.classList.remove("clear_display");
+};
